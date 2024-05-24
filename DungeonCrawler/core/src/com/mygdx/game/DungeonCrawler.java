46498f522f1afd702c2e1b91b19f87d09580c921
wellino57 <bartek4urban@gmail.com>
2024-05-24 09:37:24 +0200

    Map experiments

--- DungeonCrawler/core/src/com/mygdx/game/DungeonCrawler.java
+++ DungeonCrawler/core/src/com/mygdx/game/DungeonCrawler.java
@@ -9,7 +9,14 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
 
@@ -18,11 +25,18 @@ public class DungeonCrawler extends ApplicationAdapter {
 	Texture img;
 	Texture playerSprite;
 	Texture tile;
+	TextureRegion region;
 
 	private Rectangle player;
 	private Rectangle object;
 
+	Map level;
+	TiledMap tileMap;
+	MapLayers layers;
+	TiledMapTileLayer layer;
+
 	private OrthographicCamera camera;
+	TiledMapRenderer mapRenderer;
 	//private SpriteBatch batch;
 
 	@Override
@@ -31,21 +45,29 @@ public class DungeonCrawler extends ApplicationAdapter {
 		img = new Texture("badlogic.jpg");
 		playerSprite = new Texture(Gdx.files.internal("player.png"));
 		tile = new Texture(Gdx.files.internal("tiles.png"));
+		region = new TextureRegion(tile, 0, 0, 64, 64);
 
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
 
+		level = new Map();
+
+		tileMap = new TiledMap();
+		layers = tileMap.getLayers();
+		layer = new TiledMapTileLayer(256, 256, 16, 16);
+		layers.add(layer);
+
 		player = new Rectangle();
-		player.x = 800 / 2 - 64 / 2;
-		player.y = 20;
+		player.x = 960 / 2;
+		player.y = 540 / 2;
 		player.width = 64;
 		player.height = 64;
 
 		object = new Rectangle();
 		object.x = 100;
 		object.y = 100;
-		object.width = 128;
+		object.width = 64;
 		object.height = 64;
 
 	}
@@ -61,34 +83,52 @@ public class DungeonCrawler extends ApplicationAdapter {
 		position.x += (player.x - position.x) * lerp * Gdx.graphics.getDeltaTime();
 		position.y += (player.y - position.y) * lerp * Gdx.graphics.getDeltaTime();
 
+		mapRenderer.setView(camera);
 		camera.update();
 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
+
+		for (int y=0;y<16;y++){
+			for (int x=0;x<16;x++){
+				if(level.map[y][x]==1){
+					TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
+
+					cell.setTile(new StaticTiledMapTile(region));
+					layer.setCell(x*64, y*64, cell);
+				}
+			}
+		}
+
 		batch.draw(playerSprite, player.x, player.y);
 		batch.draw(tile,object.x,object.y);
+
 		batch.end();
 
-		if(Gdx.input.isKeyPressed(Input.Keys.W)&& Gdx.input.isKeyPressed(Input.Keys.D)){
-			player.x -= 60 * Gdx.graphics.getDeltaTime();
-			player.y -= 60 * Gdx.graphics.getDeltaTime();
+		if(!checkCollision()){
+			if(Gdx.input.isKeyPressed(Input.Keys.W)&& Gdx.input.isKeyPressed(Input.Keys.D)){
+				player.x -= 60 * Gdx.graphics.getDeltaTime();
+				player.y -= 60 * Gdx.graphics.getDeltaTime();
+			}
+			if(Gdx.input.isKeyPressed(Input.Keys.W)&& Gdx.input.isKeyPressed(Input.Keys.A)){
+				player.x += 60 * Gdx.graphics.getDeltaTime();
+				player.y -= 60 * Gdx.graphics.getDeltaTime();
+			}
+			if(Gdx.input.isKeyPressed(Input.Keys.S)&& Gdx.input.isKeyPressed(Input.Keys.D)){
+				player.x -= 60 * Gdx.graphics.getDeltaTime();
+				player.y += 60 * Gdx.graphics.getDeltaTime();
+			}
+			if(Gdx.input.isKeyPressed(Input.Keys.S)&& Gdx.input.isKeyPressed(Input.Keys.A)){
+				player.x += 60 * Gdx.graphics.getDeltaTime();
+				player.y += 60 * Gdx.graphics.getDeltaTime();
+			}
+			if(Gdx.input.isKeyPressed(Input.Keys.W)) player.y += 200 * Gdx.graphics.getDeltaTime();
+			if(Gdx.input.isKeyPressed(Input.Keys.S)) player.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(Gdx.input.isKeyPressed(Input.Keys.D)) player.x += 200 * Gdx.graphics.getDeltaTime();
+			if(Gdx.input.isKeyPressed(Input.Keys.A)) player.x -= 200 * Gdx.graphics.getDeltaTime();
+		}else{
+			System.out.println("colliding");
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.W)&& Gdx.input.isKeyPressed(Input.Keys.A)){
-			player.x += 60 * Gdx.graphics.getDeltaTime();
-			player.y -= 60 * Gdx.graphics.getDeltaTime();
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.S)&& Gdx.input.isKeyPressed(Input.Keys.D)){
-			player.x -= 60 * Gdx.graphics.getDeltaTime();
-			player.y += 60 * Gdx.graphics.getDeltaTime();
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.S)&& Gdx.input.isKeyPressed(Input.Keys.A)){
-			player.x += 60 * Gdx.graphics.getDeltaTime();
-			player.y += 60 * Gdx.graphics.getDeltaTime();
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.W)) player.y += 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.S)) player.y -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.D)) player.x += 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.A)) player.x -= 200 * Gdx.graphics.getDeltaTime();
 
 	}
 
@@ -97,5 +137,10 @@ public class DungeonCrawler extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 		playerSprite.dispose();
+	}
+
+	public boolean checkCollision () {
+		if (player.overlaps(object)) return true;
+		else return false;
 	}
 }
